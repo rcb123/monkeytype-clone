f1b0b9e7984d56e14e19e84df3c62b983e302f00
rcb123 <rbanankhah@gmail.com>
2023-01-08 20:05:34 -0800

    Small updates

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,6 +17,15 @@
   </div>
   
   <style lang="scss">
+    /* lexend-deca-regular - latin */
+    @font-face {
+      font-family: 'Lexend Deca';
+      font-style: normal;
+      font-weight: 400;
+      src: local(''), url('$static/fonts/lexend-deca-v17-latin/lexend-deca-v17-latin-regular.woff2') format('woff2'),
+        url('$static/fonts/lexend-deca-v17-latin/lexend-deca-v17-latin-regular.woff') format('woff');
+    }
+    
     h1 {
       font-family: 'Lexend Deca', sans-serif;
       font-size: 1.8rem;
@@ -25,6 +34,7 @@
     }
   
     .layout {
+      background-color: var(--bg-100);
       height: 100%;
       display: grid;
       grid-template-rows: auto 1fr;
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import path from 'path';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+			$root: path.resolve('./src'),
+			$components: path.resolve('./src/components'),
+			$static: path.resolve('./src/static'),
+		}
+	}
 };
 
 export default config;
