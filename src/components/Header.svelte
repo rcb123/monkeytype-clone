60bcde5404ff9ddf2f181cca94f9dd78b325272d
rcb123 <rbanankhah@gmail.com>
2023-01-11 23:14:45 -0800

    Update Header.svelte

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -9,35 +9,27 @@
 </script>
 
 <nav>
-	<h1 class="mt-10 text-center">🔥 sveltype</h1>
+	<h1 class="mt-10 text-center text-primary">🔥 sveltype</h1>
 
 	<select data-choose-theme class="select select-bordered w-full max-w-fit absolute top-10 right-20" tabindex="-1">
 		<option value="light">Light</option>
 		<option value="dark">Dark</option>
-		<option value="cupcake">Cupcake</option>
 		<option value="bumblebee">Bumblebee</option>
-		<option value="emerald">Emerald</option>
 		<option value="corporate">Corporate</option>
 		<option value="synthwave">Synthwave</option>
 		<option value="retro">Retro</option>
-		<option value="cyberpunk">Cyberpunk</option>
 		<option value="valentine">Valentine</option>
-		<option value="Halloween">Halloween</option>
 		<option value="garden">Garden</option>
 		<option value="forest">Forest</option>
 		<option value="aqua">Aqua</option>
 		<option value="lofi">Lofi</option>
-		<option value="pastel">Pastel</option>
 		<option value="fantasy">Fantasy</option>
-		<option value="wireframe">Wireframe</option>
 		<option value="black">Black</option>
 		<option value="luxury">Luxury</option>
 		<option value="dracula">Dracula</option>
 		<option value="cmyk">Cmyk</option>
 		<option value="autumn">Autumn</option>
 		<option value="business">Business</option>
-		<option value="acid">Acid</option>
-		<option value="lemonade">Lemonade</option>
 		<option value="night">Night</option>
 		<option value="coffee">Coffee</option>
 		<option value="winter">Winter</option>
